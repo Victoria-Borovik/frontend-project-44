7e6517c65f51c4c98c5964cbe67f1cb8a51960dc
Victoria Borovik <borovik.va111@gmail.com>
2023-04-21 17:19:44 +0300

    Add: add index.js (game engine)

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const getRandomNumber = (min, max) => Math.floor(min + Math.random() * (max - min + 1));
+
+const runGame = (gameRule, gameArray) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameRule);
+  const roundsCount = 3;
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const [question, rightAnswer] = gameArray();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== rightAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${userName}`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { getRandomNumber, runGame };
