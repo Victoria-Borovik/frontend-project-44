a22dcd469a5541a8a3d802b4ff2624babe906c1a
Victoria Borovik <borovik.va111@gmail.com>
2023-04-22 17:26:04 +0300

    Feat: add brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runGsdGame from '../src/games/gcd.js';
+
+runGsdGame();
---
+++ src/games/gcd.js
@@ -0,0 +1,23 @@
+import { getRandomNumber, runGame } from '../index.js';
+
+const gameRule = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (operand1, operand2) => {
+  const lesserOpereand = (operand1 < operand2) ? operand1 : operand2;
+  for (let i = lesserOpereand; i > 1; i -= 1) {
+    if (operand1 % i === 0 && operand2 % i === 0) {
+      return i;
+    }
+  }
+  return 1;
+};
+
+const gcd = () => {
+  const operand1 = getRandomNumber(1, 25);
+  const operand2 = getRandomNumber(1, 25);
+  const question = `${operand1} ${operand2}`;
+  const rightAnswer = getGcd(operand1, operand2);
+  return [question, rightAnswer];
+};
+
+export default () => runGame(gameRule, gcd);
