5f630d684435d9fade9d5fd7c702abcafb90a107
Victoria Borovik <borovik.va111@gmail.com>
2023-04-25 15:07:49 +0300

    Fix: fix brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,6 +3,10 @@ import { getRandomNumber, runGame } from '../index.js';
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  if (num === 1) {
+    return false;
+  }
+
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
