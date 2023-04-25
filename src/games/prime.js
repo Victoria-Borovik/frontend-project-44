a21d53f4d787db5840f17aec8574137c9202f2e1
Victoria Borovik <borovik.va111@gmail.com>
2023-04-25 11:40:57 +0300

    Feat: add brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,20 @@
+import { getRandomNumber, runGame } from '../index.js';
+
+const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const prime = () => {
+  const question = getRandomNumber();
+  const rightAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, rightAnswer];
+};
+
+export default () => runGame(gameRule, prime);
